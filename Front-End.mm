1bff1bdf170250d95a0f50ea510c25da3d60d728
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2023-12-25 16:42:59 -0300

    mapas 1

---
+++ Front-End.mm
@@ -0,0 +1,124 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Front-End" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="Freemind_Link_1513112588" CREATED="1153430895318" MODIFIED="1700950969981" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#ffffff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_super_hero_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_249848891" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" BACKGROUND_COLOR="#fff024" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_249848891" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#09387a" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fff024" BACKGROUND_COLOR="#000000">
+<font BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+<edge COLOR="#000000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#000000" BACKGROUND_COLOR="#f5131f" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f5131f"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_254323574" STYLE="narrow_hexagon" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#c3131f">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#c3131f" TRANSPARENCY="255" DESTINATION="ID_254323574"/>
+<font SIZE="11" BOLD="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#09387a" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#126cb3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#000000" BACKGROUND_COLOR="#d4e226" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#000000" BACKGROUND_COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#fff024" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#fff024" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fff024" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="HTML" POSITION="bottom_or_right" ID="ID_1994795507" CREATED="1700950937478" MODIFIED="1700950969974" HGAP_QUANTITY="28.25 pt">
+<node TEXT="Acessibilidade" ID="ID_1043806754" CREATED="1700950965559" MODIFIED="1700950966813">
+<node TEXT="WAI-ARIA" ID="ID_311716990" CREATED="1700951149942" MODIFIED="1700951152246">
+<node TEXT="O que é" FOLDED="true" ID="ID_1613977474" CREATED="1700951173249" MODIFIED="1700951320429">
+<node TEXT="A WAI-ARIA, que significa Web Accessibility Initiative - Accessible Rich Internet Applications, é um conjunto de padrões desenvolvidos pelo World Wide Web Consortium (W3C). Seu objetivo principal é melhorar a acessibilidade de conteúdos e aplicações web para pessoas com deficiências. Aqui estão alguns pontos-chave sobre o WAI-ARIA:" ID="ID_438922786" CREATED="1700952417002" MODIFIED="1700952420065"/>
+</node>
+<node TEXT="três principais features" ID="ID_688845319" CREATED="1700951320954" MODIFIED="1700951474668">
+<node TEXT="Roles (Regras)" ID="ID_1541509067" CREATED="1700951471640" MODIFIED="1700952631907" VGAP_QUANTITY="3 pt" TEXT_SHORTENED="true">
+<node TEXT="O que é?" FOLDED="true" ID="ID_1133543154" CREATED="1700952365398" MODIFIED="1700952626511">
+<node TEXT="As regras definem o tipo de elemento ou a estrutura da página. Elas indicam qual é o papel de um elemento no contexto da aplicação ou página web. Por exemplo, um elemento pode ter a role de &quot;button&quot; (botão), &quot;menu&quot;, &quot;tab&quot; (aba), etc." ID="ID_501869780" CREATED="1700952369440" MODIFIED="1700952644410"/>
+</node>
+<node TEXT="Categorias" ID="ID_611792659" CREATED="1700952500094" MODIFIED="1700952503753">
+<node TEXT="LandMarkRoles" ID="ID_1929519397" CREATED="1700952507250" MODIFIED="1700952530206">
+<node TEXT="Usada para identificar regiões significativas de uma página, facilitando a navegação." ID="ID_627935918" CREATED="1700952534410" MODIFIED="1700952535708"/>
+</node>
+<node TEXT="Structure Role" ID="ID_229026698" CREATED="1700952541389" MODIFIED="1700952542903">
+<node TEXT=" Define a estrutura ou o esquema da página." ID="ID_558549053" CREATED="1700952545370" MODIFIED="1700952546952"/>
+</node>
+<node TEXT="Widget Role" ID="ID_1094180231" CREATED="1700952552819" MODIFIED="1700952554052">
+<node TEXT="Usada para elementos interativos e controles na interface do usuário." ID="ID_185790270" CREATED="1700952558450" MODIFIED="1700952559449"/>
+</node>
+<node TEXT="Window Role" ID="ID_589636640" CREATED="1700952601738" MODIFIED="1700952603319">
+<node TEXT="Aplica-se a elementos que funcionam como janelas ou caixas de diálogo." ID="ID_204028512" CREATED="1700952606112" MODIFIED="1700952606974"/>
+</node>
+<node TEXT="Live Region Role" ID="ID_816393234" CREATED="1700952616569" MODIFIED="1700952617924">
+<node TEXT=" Usada para áreas que se atualizam dinamicamente, como feeds de notícias ou notificações." ID="ID_1897785186" CREATED="1700952621402" MODIFIED="1700952622377"/>
+</node>
+</node>
+</node>
+<node TEXT="Properties (Propriedades)" FOLDED="true" ID="ID_432983317" CREATED="1700951613838" MODIFIED="1700952460130" VGAP_QUANTITY="3 pt">
+<node TEXT="As propriedades são usadas para fornecer informações adicionais que não são explicitamente evidentes no conteúdo do elemento. Isso é crucial para os usuários que dependem de tecnologias assistivas para acessar conteúdo web.&#xa;Exemplo: &lt;input aria-required=&quot;true&quot;&gt;  indica que um campo de entrada é obrigatório." ID="ID_155934215" CREATED="1700951620201" MODIFIED="1700952658493" HGAP_QUANTITY="14.75 pt"/>
+</node>
+<node TEXT="States (Estados)" FOLDED="true" ID="ID_1541563765" CREATED="1700951636634" MODIFIED="1700951637625">
+<node TEXT=" Os estados do WAI-ARIA representam condições dinâmicas de elementos que podem mudar em resposta à interação do usuário. Por exemplo, &quot;aria-expanded&quot; indica se um item expansível está aberto ou fechado.&#xa;Importância: Esses estados ajudam as tecnologias assistivas a compreender e transmitir mudanças em elementos web. Por exemplo, quando um usuário com deficiência visual interage com um menu expansível, o leitor de tela pode informar se o menu está aberto ou fechado com base no estado &quot;aria-expanded&quot;." ID="ID_972354040" CREATED="1700951679474" MODIFIED="1700951681880"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
